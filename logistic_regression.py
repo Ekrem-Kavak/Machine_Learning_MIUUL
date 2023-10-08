7928f28237fb3719145f81e84ab97403a812fb19
Ekrem Kavak <ekvk44@gmail.com>
2023-10-08 23:30:11 +0300

    model validation

--- logistic_regression.py
+++ logistic_regression.py
@@ -244,10 +244,66 @@ plot_confusion_matrix(y, y_pred)
 print(classification_report(y, y_pred))
 
 # Accuracy: 0.78
-# Prediction: 0.74
+# Precision 0.74
 # Recall: 0.58
 # F1-score: 0.65
 
 # ROC AUC
 y_prob = log_model.predict_proba(X)[:,1]
 roc_auc_score(y, y_prob) # 0.83939
+
+# MODEL VALIDATION: HOLDOUT
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.20, random_state= 17)
+
+log_model = LogisticRegression().fit(X_train, y_train)
+
+y_pred = log_model.predict(X_test)
+y_prob = log_model.predict_proba(X_test)[:, 1]
+
+print(classification_report(y_test, y_pred))
+
+# Accuracy: 0.77
+# Precision: 0.79
+# Recall: 0.53
+# F1-score: 0.63
+
+plot_roc_curve(log_model, X_test, y_test)
+plt.title("ROC Curve")
+plt.plot([0, 1], [0, 1], 'r--')
+plt.show()
+
+# AUC
+roc_auc_score(y_test, y_prob) # 0.88
+
+
+# MODEL VALIDATION: 5-FOLD CROSS VALIDATION
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis = 1)
+
+log_model = LogisticRegression().fit(X,y)
+
+cv_results = cross_validate(log_model, X, y, cv = 5, scoring = ["accuracy", "precision", "recall", "f1", "roc_auc"])
+
+cv_results['test_accuracy'].mean()
+# Precision: 0.7721
+
+cv_results['test_precision'].mean()
+# Precision: 0.7180
+
+cv_results["test_recall"].mean()
+# Recall: 0.5747
+
+cv_results['test_f1'].mean()
+# F1-score: 0.6371
+
+cv_results['test_roc_auc'].mean()
+# AUC: 0.8327
+
+# Prediction for a new observation
+
+X.columns
+
+random_user = X.sample(1, random_state = 35)
+log_model.predict(random_user)
